ac6f7cdc719a9663fb25286476b080e39434ea45
mxmlnkn <s3495379@mailbox.tu-dresden.de>
2014-04-14 23:57:04 +0200

    added comparision to built in epsilon makros and exemplified a not so trivial division error!

--- float.c
+++ float.c
@@ -1,5 +1,6 @@
 #include <math.h>
 #include <stdio.h>
+#include <float.h>
 
 int main () {
 	float a=1,b,c;
@@ -7,7 +8,8 @@ int main () {
 		b = ldexp(1.0f, -i);
 		c = a+b;
 		if (a==c) {
-			printf("Float Error at 1+2^(-%i)\n",i);
+			printf(" Float Error at 1+2^(-%i) => epsilon:%e, FLT_EPSILON:%e\n",
+                   i,b,FLT_EPSILON);
 			break;
 		}
 	}
@@ -17,11 +19,29 @@ int main () {
 		e = ldexp(1.0d, -i);
 		f = d+e;
 		if (d==f) {
-			printf("Double Error at 1+2^(-%i)\n",i);
+			printf("Double Error at 1+2^(-%i) => epsilon:%e, DBL_EPSILON:%e\n",
+                   i,e,DBL_EPSILON);
 			break;
 		}
 	}
+    
+    /* Because of this internally units ARE needed! Calculating with SI the
+     * whole time uses up unneeded exponent "space" */
+    printf("Max Float:%e, Float Min:%e\n", FLT_MAX, FLT_MIN);
+    printf("Max Float:%i, Float Min:%i\n", FLT_MAX_EXP, FLT_MIN_EXP);
+    /* why the fuck is this working? => "FLT_MAX_EXP: The maximal exponent
+     * of a floating point value expressed in base FLT_RADIX; greater
+     * exponents are principally possible (up to 16383), but not supported
+     * in all math functions." */
+    float fer = 1.00e-42;
+    float rec = 1.0/fer;
+    printf("1.0/%e = %e\n", fer, rec);
+    /* printf shows the right number only if %e instead of %f is used.
+     * This is because %f always shows non scientific representation to a
+     * certain amount of numbers, which need to be rounded. Meaning the
+     * following will show 0, although it isn't */
+    printf("0? : %f\n", 1e-10);
+    
 	return 0;
 }
 
-//same result could be easily obtained with float.c makros: FLT_EPSILON, DBL_EPSILON.
